71572d8dbdb6db93032ab2febdc2e4a443008841
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2017-11-05 11:22:27 -0500

    Spotify client with webdriver

---
+++ spotify.rb
@@ -0,0 +1,12 @@
+require 'rspotify'
+require 'selenium-webdriver'
+RSpotify::authenticate(CLIENT_ID, API_KEY)
+# driver = Selenium::WebDriver.for :firefox
+artists = RSpotify::Artist.search(ARGV.join(' '))
+unless artists.empty?
+  artist = artists[0]
+  driver = Selenium::WebDriver.for :firefox
+  driver.navigate.to artist.external_urls["spotify"]
+else
+  puts "Cantante no encontrado"
+end
